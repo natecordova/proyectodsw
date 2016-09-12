0f6404ff3930ecaf371fa200d9a8d4d60cfbd015
natecordova <natecordovaortiz@hotmail.com>
2016-09-12 10:19:17 -0500

    Update insert.php

--- pages/insert.php
+++ pages/insert.php
@@ -11,7 +11,7 @@ $subject = "Contacto desde el sitio web";
 $message = " Name: " . $name . "\r\n City: " . $city . "\r\n Phone: " . $phone . "\r\n Email: " . $email;
 
 
-$from = "natecordovaortiz@hotmil.com";
+$from = "natecordovaortiz@hotmail.com";
 $headers = "From:" . $from . "\r\n";
 $headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
 
